e15c9e91705ae2e4828c06d7c66072293f035b38
Chiara Rotondo <chiara.r.3@live.it>
2021-11-19 18:22:10 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,29 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@GetMapping("/user/{nombre}")
+	public String getUserByName(@PathVariable String nombre) {
+		return "Hola " +nombre;
+	}
+
+	@PostMapping("/useradd")
+	public Person getPerson(@RequestBody Person person){
+		Person fields = new Person();
+		fields.setName(person.getName());
+		fields.setCity(person.getCity());
+		fields.setAge(person.getAge()+1);
+		return fields;
+	}
+}
+
---
+++ src/main/java/com/example/demo/Person.java
@@ -0,0 +1,28 @@
+package com.example.demo;
+
+public class Person{
+
+    String name;
+    String city;
+    int age;
+
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    public void setCity(String city) {
+        this.city = city;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getCity() {
+        return city;
+    }
+    public int getAge() {
+        return age;
+    }
+}
